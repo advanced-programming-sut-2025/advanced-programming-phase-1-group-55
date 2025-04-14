af170b2823fc85ee511b3e35c60cc0e0d7d90e99
arshix <93470822+arshixAB@users.noreply.github.com>
2025-04-14 20:48:22 +0330

    final commit

---
+++ src/Controller/TradeController.java
@@ -0,0 +1,31 @@
+package Controller;
+
+import enums.Result;
+
+public class TradeController {
+    public void addDollars() {
+
+    }
+
+    public Result showAllProducts() {
+        return null;
+    }
+
+    public Result showAvailabeProducts() {
+        return null;
+    }
+
+    public Result purchase(String name, int amount) {
+        return null;
+    }
+
+    public void addDollars(int amount) {
+
+    }
+
+    public void sell(String name, int amount) {
+
+    }
+
+
+}
