c125ecf8df94c87d3a43bb5da2d8d568e1d24101
arshix <93470822+arshixAB@users.noreply.github.com>
2025-04-14 18:02:27 +0330

    taghirat 2 ro menu va controller ha

--- src/model/Game.java
+++ src/model/Game.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 public class Game {
     public static User mainUser;
     public static Menu currentMenu = Menu.SignupMenu;
+    private static ArrayList<String> questionsList;
 
     public static Menu getCurrentMenu() {
         return currentMenu;
