c0fb55906d1a15a14818612fdf6e01aa0012cbcc
sinahkk <sina.hakak@gmail.com>
2025-04-14 19:46:59 +0330

    Add files via upload

---
+++ src/model/Farming.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Farming {
+    private List<Building> buildings = new ArrayList<>();
+    private List<Animal> animals = new ArrayList<>();
+
+    public void buildBuilding(Building building) {
+
+    }
+    public void buyAnimal(String name, String type) {
+
+    }
+    public void showAnimal() {
+
+    }
+    public void setFriendship(String name, int mount) {
+
+    }
+
+}
