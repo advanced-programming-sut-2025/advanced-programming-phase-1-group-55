5b18052b815b2eae6451c49423fcb71366af024b
arshix <93470822+arshixAB@users.noreply.github.com>
2025-04-14 18:28:01 +0330

    taghirat 3 ro controller va ezafe kardan weather , greenhouse

--- src/model/Game.java
+++ src/model/Game.java
@@ -7,7 +7,16 @@ import java.util.ArrayList;
 public class Game {
     public static User mainUser;
     public static Menu currentMenu = Menu.SignupMenu;
+    private ArrayList<User> AllUsers;
     private static ArrayList<String> questionsList;
+
+    public ArrayList<User> getAllUsers() {
+        return AllUsers;
+    }
+
+    public void setAllUsers(User user) {
+        AllUsers.add(user);
+    }
 
     public static Menu getCurrentMenu() {
         return currentMenu;
