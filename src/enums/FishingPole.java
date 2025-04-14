eccfb5b33a449608a246df04a169563be478e9da
Your Name <you@example.com>
2025-04-14 20:40:23 +0330

    jedi dige tamume

--- src/enums/FishingPole.java
+++
@@ -1,8 +0,0 @@
-package enums;
-
-public enum FishingPole {
-    TRAINING_ROD(0.1),
-    BAMBOO_POLE(0.5),
-    FIBERGLASS_ROD(0.8),
-    IRIDIUM_ROD(1.2);
-}
--- src/model/Fish.java
+++ src/model/Fish.java
@@ -1,7 +1,5 @@
 package model;
 
-import enums.FishingPole;
-
 import java.util.List;
 
 public class Fish {
