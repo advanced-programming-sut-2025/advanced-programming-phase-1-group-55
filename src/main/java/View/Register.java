a0ad40457cda434ce12adbb49061298c1b43f73c
arshix <93470822+arshixAB@users.noreply.github.com>
2025-05-02 12:08:42 +0330

    exit , show menu added

--- src/main/java/View/Register.java
+++ src/main/java/View/Register.java
@@ -1,11 +1,13 @@
 package View;
 
 import Controller.RegisterController;
+import enums.Menu;
 import enums.RegisterCommands;
 
 import java.util.Scanner;
 import java.util.regex.*;
 
+import static model.Game.*;
 
 public class Register extends AppView implements AppMenu {
     RegisterController controller = new RegisterController();
@@ -17,6 +19,12 @@ public class Register extends AppView implements AppMenu {
         if (Register != null) {
             System.out.println(controller.Register(Register.group("username").trim(), Register.group("password").trim(), Register.group("passwordConfirmation").trim(), Register.group("email").trim()));
 
+        } else if (input.matches("\\s*menu\\s+exit\\s*")) {
+
+            currentMenu = Menu.ExitMenu;
+        } else if (input.matches("\\s*show\\s+current\\s*menu\\s*")) {
+
+            System.out.println(currentMenu + " menu");
         }
 
 
