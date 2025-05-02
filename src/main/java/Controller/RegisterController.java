bd2544469e85712f766ea8fa762b2fb51972825d
arshix <93470822+arshixAB@users.noreply.github.com>
2025-05-02 12:29:33 +0330

    RandomPasswordGenerator added

--- src/main/java/Controller/RegisterController.java
+++ src/main/java/Controller/RegisterController.java
@@ -3,6 +3,8 @@ package Controller;
 import enums.Result;
 import model.User;
 
+import java.security.SecureRandom;
+import java.util.Random;
 import java.util.regex.Pattern;
 
 import static model.Game.*;
@@ -27,14 +29,50 @@ public class RegisterController {
             return new Result(false, "Password is not valid");
         }
         if (!isStrongPassword(password)) {
+
             return new Result(false, "Password is not strong");
         }
         if (!password.equals(passwordConfirmation)) {
             return new Result(false, "Passwords do not match");
         }
+        System.out.println("random password : " + RandomPasswordGenerator());
         return new Result(true, "Registered Successfully");
 
     }
+
+    private String RandomPasswordGenerator() {
+        final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        final String LOWER = "abcdefghijklmnopqrstuvwxyz";
+        final String DIGITS = "0123456789";
+        final String SPECIAL = "!@#$%^&*()-_=+<>?/{}[]";
+        final String ALL_CHARS = UPPER + LOWER + DIGITS + SPECIAL;
+        SecureRandom rand = new SecureRandom();
+        StringBuilder RandomPassword = new StringBuilder(10);
+        RandomPassword.append(UPPER.charAt(rand.nextInt(UPPER.length())));
+        RandomPassword.append(LOWER.charAt(rand.nextInt(LOWER.length())));
+        RandomPassword.append(DIGITS.charAt(rand.nextInt(DIGITS.length())));
+        RandomPassword.append(SPECIAL.charAt(rand.nextInt(SPECIAL.length())));
+        for (int i = 0; i < 6; i++) {
+            RandomPassword.append(ALL_CHARS.charAt(rand.nextInt(ALL_CHARS.length())));
+
+        }
+        return shuffleString(RandomPassword.toString());
+
+    }
+
+    private static String shuffleString(String input) {
+        char[] a = input.toCharArray();
+        SecureRandom rand = new SecureRandom();
+
+        for (int i = 0; i < a.length; i++) {
+            int j = rand.nextInt(a.length);
+            char temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+        return new String(a);
+    }
+
 
     private boolean isValidUsername(String username) {
         Pattern pattern = Pattern.compile("[a-zA-Z0-9]+");
