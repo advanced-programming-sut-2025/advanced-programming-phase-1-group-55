6f571e2b7f51df2c7a11660ae33c9248fbdade4a
Your Name <you@example.com>
2025-05-11 15:51:37 +0330

    background colors added to Ansicolor enum

--- src/main/java/enums/AnsiColor.java
+++ src/main/java/enums/AnsiColor.java
@@ -18,7 +18,31 @@ public enum AnsiColor {
     BRIGHT_BLUE("\u001B[94m"),
     BRIGHT_PURPLE("\u001B[95m"),
     BRIGHT_CYAN("\u001B[96m"),
-    BRIGHT_WHITE("\u001B[97m");
+    BRIGHT_WHITE("\u001B[97m"),
+    // Background colors
+    BG_BLACK("\u001B[40m"),
+    BG_RED("\u001B[41m"),
+    BG_GREEN("\u001B[42m"),
+    BG_YELLOW("\u001B[43m"),
+    BG_BLUE("\u001B[44m"),
+    BG_PURPLE("\u001B[45m"),
+    BG_CYAN("\u001B[46m"),
+    BG_WHITE("\u001B[47m"),
+
+    // Bright background colors
+    BG_BRIGHT_BLACK("\u001B[100m"),
+    BG_BRIGHT_RED("\u001B[101m"),
+    BG_BRIGHT_GREEN("\u001B[102m"),
+    BG_BRIGHT_YELLOW("\u001B[103m"),
+    BG_BRIGHT_BLUE("\u001B[104m"),
+    BG_BRIGHT_PURPLE("\u001B[105m"),
+    BG_BRIGHT_CYAN("\u001B[106m"),
+    BG_BRIGHT_WHITE("\u001B[107m"),
+
+    // Styles
+    BOLD("\u001B[1m"),
+    UNDERLINE("\u001B[4m"),
+    REVERSED("\u001B[7m");
 
     private final String code;
 
