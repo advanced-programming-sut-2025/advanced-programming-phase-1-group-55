782cfdc2317d8a40a9858699478d41e1926be6c2
sinahkk <sina.hakak@gmail.com>
2025-05-16 17:56:37 +0330

    skill

--- src/main/java/Controller/SkillController.java
+++ src/main/java/Controller/SkillController.java
@@ -1,46 +1,27 @@
-//package Controller;
-//
-//import model.Skills;
-//import model.Ability;
-//
-//import java.util.List;
-//
-//public abstract class SkillController {
-//    protected Skills skill;
-//    private static final int MAX_LEVEL = 4;
-//
-//    public SkillController(Skills skill) {
-//        this.skill = skill;
-//    }
-//
-//
-//    public void addPoints(int amount) {
-//        if (skill.getLevel() >= MAX_LEVEL) return;
-//
-//        int currentPoints = skill.getPoints();
-//        currentPoints += amount;
-//
-//        while (skill.getLevel() < MAX_LEVEL && currentPoints >= requiredPointsForNextLevel()) {
-//            currentPoints -= requiredPointsForNextLevel();
-//            skill.setLevel(skill.getLevel() + 1);
-//            onLevelUp(skill.getLevel());
-//        }
-//
-//        skill.setPoints(currentPoints);
-//    }
-//
-//    private int requiredPointsForNextLevel() {
-//        int level = skill.getLevel();
-//        return level * 100 + 50;
-//    }
-//
-//    protected abstract void onLevelUp(int newLevel);
-//
-//    public Skills getSkill() {
-//        return skill;
-//    }
-//
-//    public List<Ability> getAbilities() {
-//        return skill.getAbilities();
-//    }
-//}
+package Controller;
+
+import model.Skill;
+
+public class SkillController {
+    protected final Skill skill;
+
+    public SkillController(Skill skill) {
+        this.skill = skill;
+    }
+
+    public void addPoints(int amount) {
+        skill.changePoints(amount);
+    }
+
+    public int getLevel() {
+        return skill.getLevel();
+    }
+
+    public int getPoints() {
+        return skill.getPoints();
+    }
+
+    public Skill getSkill() {
+        return skill;
+    }
+}
