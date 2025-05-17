f282cbf0ca5ffbedaebc32e9c7165b5075068a4d
Your Name <you@example.com>
2025-05-17 13:23:41 +0330

    NPC ENUMS DONE

--- src/main/java/model/NPC/NpcType.java
+++ src/main/java/model/NPC/NpcType.java
@@ -1,5 +1,7 @@
 package model.NPC;
 
+import model.Item.Item;
+import model.Item.ItemType;
 import model.ItemOfMap;
 
 import java.util.ArrayList;
@@ -8,54 +10,84 @@ import java.util.Map;
 
 public enum NpcType {
     SEBASTIAN(
-            new HashMap<>(),
-            new HashMap<>(),
-            new HashMap<>()),
+            new HashMap<>(){{
+                put("Wool",new Item(ItemType.WOOL));
+                put(ItemType.PUMPKIN_PIE.getDisplayName(), new Item(ItemType.PUMPKIN_PIE));
+                put("pizza",new Item(ItemType.PIZZA));
+            }},
+            new HashMap<>(){{
+                put(new ReadyItem(ItemType.IRON,50),new ReadyItem(ItemType.DIAMOND,2));
+                put(new ReadyItem(ItemType.STONE,150),new ReadyItem(ItemType.QUARTZ,50));
+                put(new ReadyItem(ItemType.PUMPKIN_PIE,1),new ReadyItem(ItemType.GOLD,5000));
+            }}),
 
     ABIGAIL(
-            new HashMap<>(),
-            new HashMap<>(),
-            new HashMap<>()),
+            new HashMap<>(){{
+                put("stone",new Item(ItemType.GOLD_BAR));
+                put("iron ore", new Item(ItemType.IRON_ORE));
+                put("coffee",new Item(ItemType.COFFEE));
+            }},
+            new HashMap<>(){{
+                put(new ReadyItem(ItemType.GOLD_BAR,1),new ReadyItem(ItemType.LevelUpFriendship,1));
+                put(new ReadyItem(ItemType.PUMPKIN,1),new ReadyItem(ItemType.GOLD,500));
+                put(new ReadyItem(ItemType.WHEAT,50),new ReadyItem(ItemType.IRIDIUM_SPRINKLER,1));
+            }}),
 
     HARVEY(
-            new HashMap<>(),
-            new HashMap<>(),
-            new HashMap<>()),
+            new HashMap<>(){{
+                put(ItemType.COFFEE.getDisplayName(), new Item(ItemType.COFFEE));
+                put(ItemType.PICKLE.getDisplayName(), new Item(ItemType.PICKLE));
+                put(ItemType.BEER.getDisplayName(), new Item(ItemType.BEER));
+            }},
+            new HashMap<>(){{
+                put(new ReadyItem(ItemType.EGGPLANT,12),new ReadyItem(ItemType.GOLD,750));
+                put(new ReadyItem(ItemType.SALMON,1),new ReadyItem(ItemType.LevelUpFriendship,1));
+                put(new ReadyItem(ItemType.BEER,1),new ReadyItem(ItemType.SALAD,5));
+            }}),
 
     LEAH(
-            new HashMap<>(),
-            new HashMap<>(),
-            new HashMap<>()),
+            new HashMap<>(){{
+                put(ItemType.SALAD.getDisplayName(), new Item(ItemType.SALAD));
+                put(ItemType.GRAPES.getDisplayName(), new Item(ItemType.GRAPES));
+                put(ItemType.BEER.getDisplayName(), new Item(ItemType.BEER));
+            }},
+            new HashMap<>(){{
+                put(new ReadyItem(ItemType.HARD_WOOD,10),new ReadyItem(ItemType.GOLD,500));
+                put(new ReadyItem(ItemType.SALMON,1),new ReadyItem(ItemType.SALMON_DINNER,1));
+                put(new ReadyItem(ItemType.WOOD,200),new ReadyItem(ItemType.DELUXE_SCARECROW,1));
+            }}),
 
     ROBIN(
-            new HashMap<>(),
-            new HashMap<>(),
-            new HashMap<>());
+            new HashMap<>(){{
+                put(ItemType.IRON_BAR.getDisplayName(), new Item(ItemType.IRON_BAR));
+                put(ItemType.SPAGHETTI.getDisplayName(), new Item(ItemType.SPAGHETTI));
+                put(ItemType.WOOD.getDisplayName(), new Item(ItemType.WOOD));
+            }},
+            new HashMap<>(){{
+                put(new ReadyItem(ItemType.WOOD,80),new ReadyItem(ItemType.GOLD,1000));
+                put(new ReadyItem(ItemType.IRON_BAR,10),new ReadyItem(ItemType.BEE_HOUSE,3));
+                put(new ReadyItem(ItemType.WOOD,1000),new ReadyItem(ItemType.GOLD,25000));
+            }});
 
-    private final Map<String,ItemOfMap> favorites;
-    private final Map<String,ItemOfMap> request;
-    private final Map<String,ItemOfMap> reward;
+    private final Map<String, Item> favorites;
+    private final Map<ReadyItem,ReadyItem> exchange;
     private ArrayList<Dialog> dialogs; //todo taghir be shey dialog va ezafe kardan ertebaat baa fasl
     private  String job;
     private ArrayList<Quest> quests; //todo shey quest ezaafe beshe
-    NpcType( Map<String,ItemOfMap> favorites, Map<String,ItemOfMap> request, Map<String,ItemOfMap>reward) {
+    NpcType( Map<String,Item> favorites, Map<ReadyItem,ReadyItem> exchange) {
         this.favorites = favorites;
-        this.request = request;
-        this.reward = reward;
+        this.exchange=exchange;
     }
 
 
-    public Map<String,ItemOfMap> getFavorites() {
+    public Map<String,Item> getFavorites() {
         return favorites;
     }
 
-    public Map<String,ItemOfMap> getRequest() {
-        return request;
+    public Map<ReadyItem,ReadyItem> getRequest() {
+        return exchange;
     }
 
-    public Map<String,ItemOfMap> getReward() {
-        return reward;
-    }
 
     public ArrayList<Dialog> getDialogs() {
         return dialogs;
---
+++ src/main/java/model/NPC/ReadyItem.java
@@ -0,0 +1,30 @@
+package model.NPC;
+
+import model.Item.Item;
+import model.Item.ItemType;
+
+public class ReadyItem {
+    ItemType item;
+    int amount;
+
+    public ReadyItem(ItemType item, int amount) {
+        this.item = item;
+        this.amount = amount;
+    }
+
+    public ItemType getItem() {
+        return item;
+    }
+
+    public void setItem(ItemType item) {
+        this.item = item;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
