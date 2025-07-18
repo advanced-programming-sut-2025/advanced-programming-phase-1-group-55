4ba58fcdf8f4734782eb7e73f0985e1ead93c1e5
Your Name <you@example.com>
2025-07-18 21:06:57 +0330

    CollisionRect added

---
+++ core/src/main/java/com/StardewValley/model/Item/CollisionRect.java
@@ -0,0 +1,63 @@
+package com.StardewValley.model.Item;
+
+
+import com.badlogic.gdx.math.Rectangle;
+
+public class CollisionRect {
+    float x, y;
+    float width, height;
+    public CollisionRect(float x, float y, float width, float height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void move(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+    public Rectangle toRectangle() {
+        return new Rectangle(x, y, width, height);
+    }
+    public boolean collidesWith(CollisionRect rect){
+        return x < rect.x + rect.width && y < rect.y + rect.height && x + width > rect.x && y + height > rect.y;
+    }
+    public void updateCollisionRect(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+}
+
