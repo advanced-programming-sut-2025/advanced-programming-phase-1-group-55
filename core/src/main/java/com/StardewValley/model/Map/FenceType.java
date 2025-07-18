9ba888a0a9062f2209b387ec64b7fb3df4476af4
Your Name <you@example.com>
2025-07-18 23:00:45 +0330

    fence and fenceType classes added

---
+++ core/src/main/java/com/StardewValley/model/Map/FenceType.java
@@ -0,0 +1,15 @@
+package com.StardewValley.model.Map;
+
+import com.StardewValley.model.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
+
+public enum FenceType {
+    door(AssetManager.GATE.getTexture()),wood(AssetManager.WOOD_FENCE.getTexture()),stone(AssetManager.STONE_FENCE.getTexture());
+    private final Texture texture;
+    FenceType(Texture texture) {
+        this.texture = texture;
+    }
+    public Texture getTexture() {
+        return texture;
+    }
+}
