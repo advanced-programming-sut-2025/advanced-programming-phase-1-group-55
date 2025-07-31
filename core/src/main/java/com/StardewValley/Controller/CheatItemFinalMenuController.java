935c7947f8e0377b95f0ba6fa94899e2eb6cc37f
sinahkk <sina.hakak@gmail.com>
2025-08-01 00:37:20 +0330

    cook

--- core/src/main/java/com/StardewValley/Controller/CheatItemFinalMenuController.java
+++ core/src/main/java/com/StardewValley/Controller/CheatItemFinalMenuController.java
@@ -1,8 +1,10 @@
 package com.StardewValley.Controller;
 
 import com.StardewValley.View.newView.CheatItemFinalView;
+import com.StardewValley.enums.CookingItemType;
 import com.StardewValley.model.App;
 import com.StardewValley.model.Item.Item;
+import com.StardewValley.model.Item.ItemType;
 import com.StardewValley.model.User;
 
 import java.util.Vector;
@@ -16,18 +18,34 @@ public class CheatItemFinalMenuController {
         this.user = user;
         this.item = item;
     }
-    public void handleButton(){
-       if (view!=null) {
-           if (view.getBackButton().isChecked()){
-               view.getBackButton().setChecked(false);
-               App.gameApp.setScreen(view.getCheatItemView());
-           } else if (view.getConfirmButton().isChecked()) {
-               view.getConfirmButton().setChecked(false);
-               user.getBackPack().addItemToInventory(new Item(item.getItemType()),view.getQuantity());
-               view.setSuccessMessage("you successfully added "+view.getQuantity()+" "+item.getItemType().getDisplayName()+" to the inventory");
-           }
-       }
+    public void handleButton() {
+        if (view != null) {
+            if (view.getBackButton().isChecked()) {
+                view.getBackButton().setChecked(false);
+                App.gameApp.setScreen(view.getCheatItemView());
+            } else if (view.getConfirmButton().isChecked()) {
+                view.getConfirmButton().setChecked(false);
+                ItemType addedType = item.getItemType();
+                int quantity = view.getQuantity();
+                user.getBackPack().addItemToInventory(new Item(addedType), quantity);
+                String typeName = addedType.name();
+                if (typeName.endsWith("_RECIPE")) {
+                    String recipeName = typeName.substring(0, typeName.length() - "_RECIPE".length());
+                    try {
+                        CookingItemType cookingRecipe = CookingItemType.valueOf(recipeName);
+                        if (!user.getBackPack().getCookingRecipes().contains(cookingRecipe)) {
+                            user.getBackPack().getCookingRecipes().add(cookingRecipe);
+                            view.setSuccessMessage("You have learned the recipe for " + cookingRecipe.getProductName().getDisplayName());
+                        }
+                    } catch (IllegalArgumentException e) {
+                    }
+                }
+
+                view.setSuccessMessage("You successfully added " + quantity + " " + addedType.getDisplayName() + " to the inventory");
+            }
+        }
     }
+
     public CheatItemFinalView getView() {
         return view;
     }
