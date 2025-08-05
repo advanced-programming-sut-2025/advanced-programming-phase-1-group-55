e5ed91100068a5e4b1c31651c75a5f864721130a
Your Name <you@example.com>
2025-08-06 01:59:05 +0330

    Added fish movement types

--- core/src/main/java/com/StardewValley/Controller/FishingController.java
+++ core/src/main/java/com/StardewValley/Controller/FishingController.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.Input;
 public class FishingController {
     private FishingGameScreen view;
     private Fish fish;
+    private float time=0;
     private FishMovementPattern fishMovementPattern= FishMovementType.getRandomType().createPattern();
     public void handleButtonClicked(float delta) {
         float speed = 200;
@@ -27,11 +28,25 @@ public class FishingController {
         float top = view.getBarRect().getY() + view.getBarRect().getHeight() - view.getSimpleRect().getHeight();
         float bottom = view.getBarRect().getY();
         view.getSimpleRect().setY(Math.max(bottom, Math.min(top, view.getSimpleRect().getY())));
-        handleFishMovement(top, bottom);
+        handleFishMovement(top, bottom,delta);
     }
-    public  void handleFishMovement(float top, float bottom) {
-       view.getFishRect().setY(Math.max(bottom, Math.min(top, view.getFishRect().getY()+fishMovementPattern.getNextDeltaY())));
-       view.getFishSprite().setPosition(view.getFishRect().getX(), view.getFishRect().getY());
+    public  void handleFishMovement(float top, float bottom,float delta) {
+        time+=delta;
+        if (time>0.5f){
+            if (view.getFishRect().collidesWith(view.getSimpleRect())){
+                view.getProgressBar().setValue(view.getProgressBar().getValue()+2);
+            }else {
+                view.getProgressBar().setValue(view.getProgressBar().getValue()-1);
+            }
+            if (view.getProgressBar().getValue()<=0){
+                // todo lose
+            } else if (view.getProgressBar().getValue()>=100) {
+                //todo won
+            }
+            time=0;
+            view.getFishRect().setY(Math.max(bottom, Math.min(top, view.getFishRect().getY()+7*fishMovementPattern.getNextDeltaY())));
+            view.getFishSprite().setPosition(view.getFishRect().getX(), view.getFishRect().getY());
+        }
     }
     public FishingGameScreen getView() {
         return view;
