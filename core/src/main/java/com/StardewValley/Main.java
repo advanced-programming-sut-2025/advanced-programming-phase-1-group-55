5987e0514c6f001026ecce2be520a7642547a1cc
arshia-AB <arbehjat5@gmail.com>
2025-08-07 02:54:38 +0330

    game save 2

--- core/src/main/java/com/StardewValley/Main.java
+++ core/src/main/java/com/StardewValley/Main.java
@@ -1,16 +1,45 @@
 package com.StardewValley;
 
 import com.StardewValley.View.oldView.AppView;
+import com.StardewValley.model.GameModel;
+import com.StardewValley.model.Map.GameMap;
+import com.StardewValley.model.User;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.FileWriter;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
-        AppView program = new AppView();
-        program.run();
+        try {
+            User user = new User();
+            GameMap map = new GameMap();
 
+            GameModel gameModel = new GameModel(user, map);
+
+
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            String filename = "game_save.json";
+
+
+            try (FileWriter writer = new FileWriter(filename)) {
+                gson.toJson(gameModel, writer);
+            }
+
+
+            try (Reader reader = Files.newBufferedReader(Paths.get(filename))) {
+                GameModel loadedGameModel = gson.fromJson(reader, GameModel.class);
+                System.out.println("Game loaded! Current User: " + loadedGameModel.getCurrentUser());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
-
 
